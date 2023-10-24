74848a01b74c40a060ae46586ba2abd225cd0351
Богдан Безуглый <winzar06031977@gmail.com>
2023-10-24 18:46:32 +0300

    Modified 14 task

--- 14.py
+++ 14.py
@@ -5,3 +5,6 @@ print("Array: " + str(arr))
 print("Avarage: " + str(arr.mean()))
 print("Avarage elements in every row: " + str(arr.mean(axis=1)))
 print("Avarage elements in every column: " + str(arr.mean(axis=0)))
+arr[arr >= 0] = 5
+arr[arr < 0] = 3
+print("Array after changes: " + str(arr))
